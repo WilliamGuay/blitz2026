47dd120defa3ca5c64443217ba55f00986228e55
William Guay <guay.william01@gmail.com>
2026-01-17 11:22:07 -0500

    ajout prise de cibles

--- python/bot.py
+++ python/bot.py
@@ -1,27 +1,50 @@
 import random
 from game_message import *
 
+ROLES = []
 
 class Bot:
+    
     def __init__(self):
-        print("Initializing your super mega duper bot")
+        self.landBase = []      
+        self.targetNutrients = []
+        self.totalPossibleIncome = 0
+        
+    def getNextLandToCapture(self, game_message, team_id):
+        ownGrid = game_message.world.ownershipGrid
+        for n in self.landBase:
+            if ownGrid[n[0], n[1]] != team_id:
+                return n
+        
+        return (-1, -1)
 
     def get_next_move(self, game_message: TeamGameState) -> list[Action]:
-        """
-        Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
-        """
         actions = []
-
         my_team: TeamInfo = game_message.world.teamInfos[game_message.yourTeamId]
+
+
         if len(my_team.spawners) == 0:
             actions.append(SporeCreateSpawnerAction(sporeId=my_team.spores[0].id))
 
+        if len(self.landBase) == 0 and not len(my_team.spawners) == 0:
+            nmap = game_message.world.map.nutrientGrid #Not the other one
+            initSpawn = my_team.spawners[0].position
+            for x in range(len(nmap)):
+                for y in range(len(nmap[x])):
+                    if nmap[x][y] > 0:
+                        self.landBase.append((x, y))
+                        self.totalPossibleIncome += nmap[x][y]
+                        
+            self.landBase.sort(key=lambda pos: abs(pos[0] - initSpawn.x) + abs(pos[1] - initSpawn.y))
+            
         elif len(my_team.spores) == 0:
             actions.append(
                 SpawnerProduceSporeAction(spawnerId=my_team.spawners[0].id, biomass=20)
             )
 
         else:
+            
+            
             if my_team.nutrients >= 10:
                 actions.append(SpawnerProduceSporeAction(spawnerId=my_team.spawners[0].id, biomass=10))
                 
